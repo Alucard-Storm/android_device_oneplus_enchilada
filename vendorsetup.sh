aee094f5b28d48279faccdee5dcf3503dba80f53
Alucard-Storm <supersayan1000@gmail.com>
2025-04-14 12:04:52 +0530

    vendorsetup: fetch los 22.2 branch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,17 @@
 echo "cloning common tree"
-git clone https://github.com/LineageOS/android_device_oneplus_sdm845-common.git -b lineage-22.1 device/oneplus/sdm845-common --depth=1
+git clone https://github.com/LineageOS/android_device_oneplus_sdm845-common.git -b lineage-22.2 device/oneplus/sdm845-common --depth=1
 
 echo "cloning kernel"
-git clone https://github.com/LineageOS/android_kernel_oneplus_sdm845.git -b lineage-22.1 kernel/oneplus/sdm845 --depth=1
+git clone https://github.com/LineageOS/android_kernel_oneplus_sdm845.git -b lineage-22.2 kernel/oneplus/sdm845 --depth=1
 
 echo "cloning vendor"
-git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_enchilada -b lineage-22.1 vendor/oneplus/enchilada --depth=1
+git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_enchilada -b lineage-22.2 vendor/oneplus/enchilada --depth=1
 
 echo "cloning common vendor"
-git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_sdm845-common.git -b lineage-22.1 vendor/oneplus/sdm845-common --depth=1
+git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_sdm845-common.git -b lineage-22.2 vendor/oneplus/sdm845-common --depth=1
 
 echo "cloning extra repos"
-git clone https://github.com/LineageOS/android_hardware_oneplus.git -b lineage-22.1 hardware/oneplus --depth=1
+git clone https://github.com/LineageOS/android_hardware_oneplus.git -b lineage-22.2 hardware/oneplus --depth=1
 git clone https://github.com/xiaomi-haydn-devs/priv_keys.git vendor/oneplus/priv-keys
 
 echo "deleting the clone script, BYE!"
