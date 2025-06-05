8905ee4e08c62fd00ad17ce6b06f74b63dd09ead
Alucard Stormbringer <supersayan1000@gmail.com>
2025-06-05 22:53:30 +0530

    enchilada: vendorsetup: clone submodules with kernel
    Clone KSU-Next along with the kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ echo "cloning common tree"
 git clone https://github.com/LineageOS/android_device_oneplus_sdm845-common.git -b lineage-22.2 device/oneplus/sdm845-common --depth=1
 
 echo "cloning kernel"
-git clone https://github.com/Alucard-Storm/android_kernel_oneplus_sdm845.git -b lineage-22.2 kernel/oneplus/sdm845 --depth=1
+git clone https://github.com/Alucard-Storm/android_kernel_oneplus_sdm845.git -b lineage-22.2 kernel/oneplus/sdm845 --depth=1 --recursive
 
 echo "cloning vendor"
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_enchilada -b lineage-22.2 vendor/oneplus/enchilada --depth=1
