cd4ccfa8376093c0d009f199350fc6799403d60a
Alucard-Storm <supersayan1000@gmail.com>
2025-06-30 22:57:53 +0530

    enchilada: vendorsetup: track own fork

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 echo "cloning common tree"
-git clone https://github.com/LineageOS/android_device_oneplus_sdm845-common.git -b lineage-22.2 device/oneplus/sdm845-common --depth=1
+git clone https://github.com/Alucard-Storm/android_device_oneplus_sdm845-common.git -b lineage-23 device/oneplus/sdm845-common --depth=1
 
 echo "cloning kernel"
 git clone https://github.com/Alucard-Storm/android_kernel_oneplus_sdm845.git -b lineage-22.2 kernel/oneplus/sdm845 --depth=1 --recursive
@@ -8,7 +8,7 @@ echo "cloning vendor"
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_enchilada -b lineage-22.2 vendor/oneplus/enchilada --depth=1
 
 echo "cloning common vendor"
-git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_sdm845-common.git -b lineage-22.2 vendor/oneplus/sdm845-common --depth=1
+git clone https://github.com/Alucard-Storm/proprietary_vendor_oneplus_sdm845-common.git -b lineage-23 vendor/oneplus/sdm845-common --depth=1
 
 echo "cloning extra repos"
 git clone https://github.com/LineageOS/android_hardware_oneplus.git -b lineage-22.2 hardware/oneplus --depth=1
